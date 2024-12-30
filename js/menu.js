f5db8e5a95b789f2eb70b2ef6b8b51b9e4301685
Keyang Zuo <zuokeyangtx@163.com>
2024-12-30 11:39:53 +0800

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const menu = document.querySelector('.menu');
+
+hamburgerBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
