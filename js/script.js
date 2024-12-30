bad521bddf1779e4da02ff24356f6e8349e06e2f
Keyang Zuo <zuokeyangtx@163.com>
2024-12-30 12:01:01 +0800

    Delete js/script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerBtn = document.querySelector('.hamburger - btn');
-const menu = document.querySelector('.menu');
-
-hamburgerBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
